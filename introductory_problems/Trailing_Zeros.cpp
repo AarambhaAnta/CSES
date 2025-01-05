61053a1182d5d52c7ebe972f165b997859c6489c
AarambhaAnta <aarambha108@gmail.com>
2025-01-05 23:32:34 +0530

    introductory_problems-> trailing_zeros solution

---
+++ introductory_problems/Trailing_Zeros.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+#define vi vector<int>
+#define ll long long
+using namespace std;
+
+void solve()
+{
+  int n;
+  cin >> n;
+  int ans = 0;
+  while (n)
+  {
+    ans += n / 5;
+    n /= 5;
+  }
+  cout << ans << '\n';
+}
+int main()
+{
+  int t = 1;
+  // cin>>t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
