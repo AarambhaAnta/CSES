1b10110208f014dfb6e928088706178ed6e09654
AarambhaAnta <aarambha108@gmail.com>
2025-01-06 23:03:51 +0530

    introductory_problems-> coin_piles solution

---
+++ introductory_problems/Coin_Piles.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+#define vi vector<int>
+#define ll long long
+using namespace std;
+
+void solve(){
+  int a, b;
+  cin >> a >> b;
+  if((a+b)%3==0 && a<=2*b && b<=2*a)
+    cout << "YES\n";
+  else
+    cout << "NO\n";
+}
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
