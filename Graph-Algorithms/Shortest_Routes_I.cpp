3a2eba32e90f12e8657a2144846e40c08ba75b31
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:46:34 +0530

    feat: solve Shortest_Routes_I

--- Graph-Algorithms/Shortest_Routes_I.cpp
+++ Graph-Algorithms/Shortest_Routes_I.cpp
@@ -1,162 +1,50 @@
-
 #include <bits/stdc++.h>
-using namespace std;
-
-// Type Aliases
-#define int long long
-#define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
+typedef long long ll;
+typedef pair<ll, ll> pii;
+
+signed main() {
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-}
 
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
+    ll n, m;
+    cin >> n >> m;
+    vector<vector<pii>> adj(n + 1);
+    for (ll i = 0; i < m; i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({w, v});
     }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
 
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-struct Node {
-    int u, w;
-    bool operator>(const Node& other) const {
-        return w > other.w;
-    }
-};
-
-const int maxn = 1e5 + 5;
-int n, m;
-vector<pii> g[maxn];
-int dis[maxn];
-
-void dijkstra(Node start) {
-    priority_queue<Node, vector<Node>, greater<Node>> pq;
-    dis[start.u] = 0;
-    pq.push({start.u, 0});
+    ll inf = 1e18;
+    vector<ll> dis(n + 1, inf);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    dis[1] = 0;
+    pq.push({0, 1});
     while (!pq.empty()) {
-        Node node = pq.top();
+        auto [d, u] = pq.top();
         pq.pop();
-        int u = node.u;
-        int w = node.w;
-        if (w > dis[u])
+        if (d > dis[u])
             continue;
 
-        for (auto [v, weight] : g[u]) {
-            if (dis[v] > dis[u] + weight) {
+        for (auto [weight, v] : adj[u]) {
+            if (dis[u] + weight < dis[v]) {
                 dis[v] = dis[u] + weight;
-                pq.push({v, dis[v]});
+                pq.push({dis[v], v});
             }
         }
     }
-}
-void test() {
-    // your solution for each testcase
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        g[u].pb(mp(v, w));
+    for (ll i = 1; i <= n; i++) {
+        cout << dis[i] << " ";
     }
-    fill(dis, dis + n + 1, INF);
-    dijkstra({1, 0});
 
-    for (int i = 1; i <= n; i++) {
-        cout << dis[i] << sp;
-    }
-    cout << nl;
-}
-
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
     return 0;
 }
