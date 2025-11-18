433553a7b86dfe0d0ea7fa13819b454652f1d56a
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:28 +0530

    feat: solve Flight_Discount

--- Graph-Algorithms/Flight_Discount.cpp
+++ Graph-Algorithms/Flight_Discount.cpp
@@ -1,162 +1,49 @@
-
 #include <bits/stdc++.h>
-using namespace std;
-
-// Type Aliases
-#define int long long
-#define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+signed main() {
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-}
 
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
+    ll n, m;
+    cin >> n >> m;
+    vector<vector<pll>> adj(2 * n + 1);
+    for (int i = 0; i < m; i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({w, v});
+        adj[u + n].push_back({w, v + n});
+        adj[u].push_back({w / 2, v + n});
     }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
 
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-const int maxn = 1e5 + 5;
-vii g[maxn];
-int dis[maxn][2];
-void dijkstra(int src) {
-    dis[src][0] = 0;
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
-                   greater<tuple<int, int, int>>>
-        pq;
-    pq.push({0, src, 0});
+    ll inf = 1e18;
+    vector<ll> dis(2 * n + 1, inf);
+    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    dis[1] = 0;
+    pq.push({0, 1});
     while (!pq.empty()) {
-        auto [d, u, used] = pq.top();
+        auto [d, u] = pq.top();
         pq.pop();
-        if (d > dis[u][used])
+        if (dis[u] < d)
             continue;
-        for (auto [v, w] : g[u]) {
-            if (dis[v][used] > dis[u][used] + w) {
-                dis[v][used] = dis[u][used] + w;
-                pq.push({dis[v][used], v, used});
-            }
-            if (used == 0) {
-                int discounted = dis[u][used] + w / 2;
-                if (dis[v][1] > discounted) {
-                    dis[v][1] = discounted;
-                    pq.push({dis[v][1], v, 1});
-                }
+        for (auto [w, v] : adj[u]) {
+            ll new_dis = dis[u] + w;
+            if (new_dis < dis[v]) {
+                dis[v] = new_dis;
+                pq.push({new_dis, v});
             }
         }
     }
-}
-void test() {
-    // your solution for each testcase
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        g[u].pb({v, w});
-    }
-
-    for (int i = 1; i <= n; ++i) {
-        dis[i][0] = INF;
-        dis[i][1] = INF;
-    }
-
-    dijkstra(1);
-
-    cout << dis[n][1] << nl;
-}
-
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    cout << min(dis[n], dis[2 * n]) << endl;
     return 0;
 }
