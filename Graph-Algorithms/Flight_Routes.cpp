7438c21eb729921d47aa3cc848bf83b1206288a6
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:34 +0530

    feat: solve Flight_Routes

--- Graph-Algorithms/Flight_Routes.cpp
+++ Graph-Algorithms/Flight_Routes.cpp
@@ -1,152 +1,54 @@
-
 #include <bits/stdc++.h>
-using namespace std;
-
-// Type Aliases
-#define int long long
-#define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
+template <typename T>
+using rev_pq = priority_queue<T, vector<T>, greater<T>>;
+
+typedef long long ll;
+
+signed main() {
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-}
 
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-const int maxn = 1e5 + 5;
-
-int n, m, k;
-vii g[maxn];
-
-vi k_shortest_path(int src, int des, int k) {
-    vector<vi> dis(n + 1);
-    priority_queue<pii, vii, greater<pii>> pq;
-    pq.push({0, src});
-    while (!pq.empty()) {
-        auto node = pq.top();
-        pq.pop();
-        int d = node.ff;
-        int u = node.ss;
-        if ((int)dis[u].size() >= k)
-            continue;
-        dis[u].pb(d);
-        for (auto child : g[u]) {
-            int v = child.ff, w = child.ss;
-            if ((int)dis[v].size() < k) {
-                pq.push({d + w, v});
-            }
-        }
-    }
-    return dis[des];
-}
-void test() {
-    // your solution for each testcase
+    int n, m, k;
     cin >> n >> m >> k;
+    vector<vector<pair<int, int>>> adj(n + 1);
     for (int i = 0; i < m; i++) {
         int u, v, w;
         cin >> u >> v >> w;
-        g[u].pb({v, w});
+        adj[u].push_back({w, v});
     }
 
-    vi costs = k_shortest_path(1, n, k);
-    vout(costs);
-}
+    vector<int> dis(k + 1);
+    vector<int> vis(n + 1);
+    rev_pq<pair<int, int>> pq;
+    pq.push({0, 1});
 
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if (vis[u] >= k)
+            continue;
+        if (u == n) {
+            dis[vis[u]] = d;
+        }
+        ++vis[u];
+        for (auto [w, v] : adj[u]) {
+            pq.push({d + w, v});
+        }
+    }
+
+    for (int i = 0; i < k; i++) {
+        cout << dis[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
