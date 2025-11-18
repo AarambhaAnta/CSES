c6ae04eeccadc636c8a5d92695e1391da1970b66
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:17 +0530

    feat: solve Course_Schedule

--- Graph-Algorithms/Course_Schedule.cpp
+++ Graph-Algorithms/Course_Schedule.cpp
@@ -1,159 +1,54 @@
-
 #include <bits/stdc++.h>
-using namespace std;
-
-// Type Aliases
-#define int long long
-#define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
+typedef long long ll;
+
+signed main() {
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-}
 
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-const int maxn = 1e5 + 5;
-int n, m;
-vi g[maxn];
-int indeg[maxn];
-vi khans_algo() {
-    vi topo;
-    queue<int> q;
-    for (int i = 1; i <= n; i++) {
-        if (indeg[i] == 0)
-            q.push(i);
-    }
-
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        topo.pb(u);
-        for (int v : g[u]) {
-            indeg[v]--;
-            if (indeg[v] == 0)
-                q.push(v);
-        }
-    }
-
-    if ((int)topo.size() != n)
-        return {};
-    return topo;
-}
-void test() {
-    // your solution for each testcase
+    int n, m;
     cin >> n >> m;
-    fill(indeg, indeg + n + 1, 0);
+    vector<vector<int>> adj(n + 1);
+    vector<int> indeg(n + 1);
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        g[u].pb(v);
+        adj[u].push_back(v);
         indeg[v]++;
     }
 
-    vi topo = khans_algo();
-    if (topo.empty()) {
-        cout << "IMPOSSIBLE" << nl;
-    } else {
-        vout(topo);
+    vector<int> no_incoming;
+    for (int u = 1; u <= n; u++) {
+        if (indeg[u] == 0) {
+            no_incoming.push_back(u);
+        }
     }
-}
 
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    for (int i = 0; i < no_incoming.size(); i++) {
+        int u = no_incoming[i];
+        for (int v : adj[u]) {
+            --indeg[v];
+            if (indeg[v] == 0) {
+                no_incoming.push_back(v);
+            }
+        }
+    }
+
+    if(no_incoming.size()<n){
+        cout << "IMPOSSIBLE\n";
+        return 0;
+    }
+    for(int x:no_incoming){
+        cout << x << " ";
+    }
+
     return 0;
 }
