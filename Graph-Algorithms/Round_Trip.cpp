dfdf13fee5e855ace91da14ffb5f74f5bd06051e
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:46:20 +0530

    feat: solve Round_Trip

--- Graph-Algorithms/Round_Trip.cpp
+++ Graph-Algorithms/Round_Trip.cpp
@@ -1,173 +1,80 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-// Type Aliases
-#define int long long
 #define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-const int N = 1e5 + 5;
+// inital
 int n, m;
-vi g[N];
-int vis[N];
-int parent[N];
-vi loop;
-bool is_loop = false;
-void dfs(int node, int par) {
-    vis[node] = 2;
-    parent[node] = par;
-    for (int v : g[node]) {
-        if (v == par)
+vector<vector<int>> g;
+vector<bool> vis;
+vector<int> par;
+vector<int> path;
+bool has_cycle = false;
+
+// helper
+void dfs(int src, int p) {
+    vis[src] = 1;
+    par[src] = p;
+    for (int nei : g[src]) {
+        if (nei == p)
             continue;
-        if (vis[v] == 1) {
-            dfs(v, node);
-        } else if (vis[v] == 2) {
-            if (loop.empty()) {
-                int cur = node;
-                while (cur != v) {
-                    loop.pb(cur);
-                    cur = parent[cur];
-                }
-                loop.pb(cur);
-                loop.pb(node);
-                is_loop = true;
-                return;
+        if (!vis[nei]) {
+            dfs(nei, src);
+        } else {
+            int cur = src;
+            while (cur != nei) {
+                path.push_back(cur);
+                cur = par[cur];
             }
+            path.push_back(nei);
+            path.push_back(src);
+            has_cycle = true;
         }
+        if (has_cycle)
+            return;
     }
-    vis[node] = 3;
 }
-void test() {
-    // your solution for each testcase
+void solve() {
+    // input
     cin >> n >> m;
+    g.resize(n + 1);
+    par.resize(n + 1, 0);
+    vis.resize(n + 1, 0);
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        g[u].pb(v);
-        g[v].pb(u);
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
-    fill(vis, vis + n + 1, 1);
-    fill(parent, parent + n + 1, 0);
 
+    // process
     for (int i = 1; i <= n; i++) {
-        if (vis[i] == 1) {
-            dfs(i, -1);
-            if (is_loop)
+        if (!vis[i]) {
+            dfs(i, 0);
+            if (has_cycle)
                 break;
         }
     }
 
-    if (!is_loop) {
+    // output
+    if (!has_cycle) {
         cout << "IMPOSSIBLE" << nl;
         return;
-    } else {
-        cout << loop.size() << nl;
-        vout(loop);
     }
+    cout << path.size() << nl;
+    for (int p : path) {
+        cout << p << " ";
+    }
+    cout << nl;
 }
 
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
     int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    // cin >> tc;
+    while (tc--) {
+        solve();
+    }
     return 0;
 }
