2fce34b64d165330fe5f4439f91c04e6e3756bbd
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:49 +0530

    feat: solve High_Score

--- Graph-Algorithms/High_Score.cpp
+++ Graph-Algorithms/High_Score.cpp
@@ -1,168 +1,73 @@
-
 #include <bits/stdc++.h>
-using namespace std;
-
-// Type Aliases
-#define int long long
-#define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
+using namespace std;
+using namespace __gnu_pbds;
 template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
+typedef long long ll;
 
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-int n, m;
-struct Edge {
-    int u, v, w;
-};
-vector<Edge> edges;
-vi dis;
-vb isPositiveCycle;
-
-void bellmanFord(int node) {
-    dis[node] = 0;
-    for (int i = 1; i < n; i++) {
-        for (auto edge : edges) {
-            int u = edge.u, v = edge.v, w = edge.w;
-            if (dis[u] > -INF && dis[v] < dis[u] + w) {
-                dis[v] = dis[u] + w;
-            }
-        }
-    }
-
-    for (auto edge : edges) {
-        int u = edge.u, v = edge.v, w = edge.w;
-        if (dis[u] > -INF && dis[v] < dis[u] + w) {
-            isPositiveCycle[v] = true;
-        }
-    }
-
-    for (int i = 1; i < n; i++) {
-        for (auto edge : edges) {
-            if (isPositiveCycle[edge.u]) {
-                isPositiveCycle[edge.v] = true;
-            }
-        }
-    }
-}
-
-void test() {
-    // your solution for each testcase
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        edges.pb({u, v, w});
-    }
-
-    dis.assign(n + 1, -INF);
-    isPositiveCycle.assign(n + 1, false);
-    bellmanFord(1);
-    int ans = dis[n];
-    if (ans <= -INF || ans >= INF || isPositiveCycle[n]) {
-        cout << -1 << nl;
-    } else {
-        cout << ans << nl;
-    }
-}
-
-//======================================== Driver Code ========================================//
 signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    ll n, m;
+    cin >> n >> m;
+    ll inf = 1e18;
+    vector<tuple<ll, ll, ll>> edges;
+    vector<vector<ll>> g(n + 1), rev_g(n + 1);
+    vector<ll> dis(n + 1, inf);
+
+    for (ll i = 0; i < m; i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({u, v, -w});
+        g[u].push_back(v);
+        rev_g[v].push_back(u);
+    }
+
+    const size_t nn = n;
+    function<vector<bool>(ll, vector<vector<ll>>)> bfs = [&](ll src, vector<vector<ll>> graph) {
+        vector<bool> can_reach(nn + 1, false);
+        can_reach[src] = true;
+        queue<ll> q;
+        q.push(src);
+        while (!q.empty()) {
+            ll u = q.front();
+            q.pop();
+            for (ll v : graph[u]) {
+                if (!can_reach[v]) {
+                    can_reach[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+        return can_reach;
+    };
+    vector<bool> can_reach = bfs(1, g);
+    vector<bool> can_reach_from_n = bfs(n, rev_g);
+    for (size_t i = 0; i <= nn; i++) {
+        can_reach[i] = can_reach[i] && can_reach_from_n[i];
+    }
+
+    dis[1] = 0;
+
+    for (ll i = 1; i <= n; i++) {
+        for (auto [u, v, w] : edges) {
+            if (!can_reach[u] || !can_reach[v])
+                continue;
+            ll new_dis = min(dis[v], dis[u] + w);
+            if (i == n && new_dis < dis[v]) {
+                cout << -1 << endl;
+                return 0;
+            }
+            dis[v] = new_dis;
+        }
+    }
+
+    cout << -dis[n] << endl;
     return 0;
 }
