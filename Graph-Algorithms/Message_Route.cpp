09e3e2eb2714520dd2a887fb594f9372cf79f270
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:46:08 +0530

    feat: solve Message_Route

--- Graph-Algorithms/Message_Route.cpp
+++ Graph-Algorithms/Message_Route.cpp
@@ -1,170 +1,74 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-// Type Aliases
-#define int long long
 #define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-const int N = 1e5 + 5;
+// initial
 int n, m;
-vi g[N];
-int dis[N], par[N];
-bool vis[N];
+vector<vector<int>> g;
+vector<int> dis;
+vector<int> par;
+const int inf = 1e9;
+
+// helper
 void bfs(int src) {
-    dis[src] = 1;
-    par[src] = -1;
+    dis[src] = 0;
     queue<int> q;
     q.push(src);
     while (!q.empty()) {
         int u = q.front();
         q.pop();
-        if (vis[u])
-            continue;
-        vis[u] = true;
-        for (int v : g[u]) {
-            if (dis[v] > dis[u] + 1) {
-                dis[v] = dis[u] + 1;
-                q.push(v);
-                par[v] = u;
+        for (int nei : g[u]) {
+            if (dis[nei] > dis[u] + 1) {
+                dis[nei] = dis[u] + 1;
+                par[nei] = u;
+                q.push(nei);
             }
         }
     }
 }
-void test() {
-    // your solution for each testcase
+void solve() {
+    // input
     cin >> n >> m;
-    for (int i = 0; i < m; i++) {
+    g.resize(n + 1);
+    dis.resize(n + 1, inf);
+    par.resize(n + 1);
+    for (int i = 1; i <= m; i++) {
         int u, v;
         cin >> u >> v;
-        g[u].pb(v);
-        g[v].pb(u);
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
 
-    fill(dis, dis + n + 1, INF);
-    fill(vis, vis + n + 1, false);
-    fill(par, par + n + 1, -1);
-
+    // process
     bfs(1);
-    if (dis[n] == INF) {
+    
+    // output
+    if (dis[n] == inf) {
         cout << "IMPOSSIBLE" << nl;
         return;
     }
-
+    vector<int> res;
     int cur = n;
-    vi res;
-    while (cur != -1) {
-        res.pb(cur);
+    while (cur != 1) {
+        res.push_back(cur);
         cur = par[cur];
     }
-
-    reverse(all(res));
-    cout << dis[n] << nl;
-    vout(res);
+    res.push_back(1);
+    cout << dis[n] + 1 << nl;
+    reverse(res.begin(), res.end());
+    for (int r : res) {
+        cout << r << " ";
+    }
 }
 
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
     int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    // cin >> tc;
+    while (tc--) {
+        solve();
+    }
     return 0;
 }
