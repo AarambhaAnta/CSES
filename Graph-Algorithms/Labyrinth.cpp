cf590d55d10411329f5798c403f0c53ee8dada1c
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:54 +0530

    feat: solve Labyrinth

--- Graph-Algorithms/Labyrinth.cpp
+++ Graph-Algorithms/Labyrinth.cpp
@@ -1,182 +1,92 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-// Type Aliases
-#define int long long
 #define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+// #define int long long
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
+// initial
 int n, m;
-vector<string> grid;
-vector<vi> dis;
-vector<vii> par;
+vector<string> g;
+int row[] = {-1, 1, 0, 0};
+int col[] = {0, 0, -1, 1};
+char dir[] = {'U', 'D', 'L', 'R'};
+const int inf = 1e9;
 
-void bfs(pii src) {
-    dis[src.ff][src.ss] = 0;
-    par[src.ff][src.ss] = mp(-1, -1);
+// helper
+vector<vector<int>> dis;
+vector<vector<pair<int, int>>> par;
 
-    queue<pii> q;
+bool is_valid(int nx, int ny) {
+    return nx >= 0 && ny >= 0 && nx < n && ny < m;
+}
+void bfs(pair<int, int> src) {
+    dis[src.first][src.second] = 0;
+    par[src.first][src.second] = {-1, -1};
+    queue<pair<int, int>> q;
     q.push(src);
     while (!q.empty()) {
         auto [x, y] = q.front();
         q.pop();
-
         for (int i = 0; i < 4; i++) {
-            int ni = x + dx[i], nj = y + dy[i];
-            if (ni >= 0 && nj >= 0 && ni < n && nj < m && grid[ni][nj] != '#' &&
-                dis[ni][nj] > dis[x][y] + 1) {
-                dis[ni][nj] = dis[x][y] + 1;
-                par[ni][nj] = mp(x, y);
-                q.push(mp(ni, nj));
+            int nx = x + row[i], ny = y + col[i];
+            if (is_valid(nx, ny) && g[nx][ny] != '#' && dis[nx][ny] > dis[x][y] + 1) {
+                dis[nx][ny] = dis[x][y] + 1;
+                par[nx][ny] = {x, y};
+                q.push({nx, ny});
             }
         }
     }
 }
-void test() {
-    // your solution for each testcase
+void solve() {
+    // input
     cin >> n >> m;
-    grid.assign(n, "");
-    pii src = {-1, -1}, des = {-1, -1};
+    g.resize(n);
+    pair<int, int> src, des;
     for (int i = 0; i < n; i++) {
-        cin >> grid[i];
-        if (src.ss == -1 && grid[i].find('A') != string::npos) {
-            src = mp(i, grid[i].find('A'));
+        cin >> g[i];
+        size_t sj = g[i].find('A'), dj = g[i].find('B');
+        if (sj != string::npos) {
+            src = {i, sj};
         }
-        if (des.ss == -1 && grid[i].find('B') != string::npos) {
-            des = mp(i, grid[i].find('B'));
+        if (dj != string::npos) {
+            des = {i, dj};
         }
     }
 
-    dis.assign(n, vi(m, INF));
-    par.assign(n, vii(m, {-1, -1}));
-
+    // process
+    dis = vector<vector<int>>(n, vector<int>(m, inf));
+    par = vector<vector<pair<int, int>>>(n, vector<pair<int, int>>(m, {-1, -1}));
     bfs(src);
 
-    if (dis[des.ff][des.ss] == INF) {
+    // output
+    if (dis[des.first][des.second] == inf) {
         cout << "NO" << nl;
         return;
     }
-
     cout << "YES" << nl;
-    cout << dis[des.ff][des.ss] << nl;
-
+    cout << dis[des.first][des.second] << nl;
     string res;
-    pii cur = des;
+    pair<int, int> cur = des;
     while (cur != src) {
-        pii p = par[cur.ff][cur.ss];
+        pair<int, int> p = par[cur.first][cur.second];
         for (int i = 0; i < 4; i++) {
-            if (p.ff + dx[i] == cur.ff && p.ss + dy[i] == cur.ss) {
-                res.pb(dir[i]);
-                break;
+            if (make_pair(p.first + row[i], p.second + col[i]) == cur) {
+                res.push_back(dir[i]);
+                cur = p;
             }
         }
-        cur = p;
     }
-    reverse(all(res));
+    reverse(res.begin(), res.end());
     cout << res << nl;
 }
 
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
     int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    // cin >> tc;
+    while (tc--) {
+        solve();
+    }
     return 0;
 }
