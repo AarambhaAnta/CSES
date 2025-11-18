91881d19f24d8a9dd0fee400a235520880d76307
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:00 +0530

    feat: solve Building_Teams

--- Graph-Algorithms/Building_Teams.cpp
+++ Graph-Algorithms/Building_Teams.cpp
@@ -1,159 +1,63 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-// Type Aliases
-#define int long long
 #define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-const int N = 1e5 + 5;
-vi g[N];
-int color[N];
+// inital
+int n, m;
+vector<vector<int>> g;
 bool is_bipartite = true;
-void dfs(int node, int c) {
-    color[node] = c;
-    for (int v : g[node]) {
-        if (color[v] == c) {
+vector<int> colors;
+
+// helper
+void dfs(int src, int c) {
+    colors[src] = c;
+    for (int nei : g[src]) {
+        if (colors[nei] == 0) {
+            dfs(nei, 3 - c);
+        } else if (colors[nei] == c) {
             is_bipartite = false;
             return;
-        } else if (color[v] == 0) {
-            dfs(v, 3 - c);
         }
     }
 }
-void test() {
-    // your solution for each testcase
-    int n, m;
+void solve() {
+    // input
     cin >> n >> m;
+    g.resize(n + 1);
+    colors.resize(n + 1, 0);
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        g[u].pb(v);
-        g[v].pb(u);
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
 
-    fill(color, color + n + 1, 0);
-
-    for (int i = 1; i <= n; i++) {
-        if (color[i] == 0) {
+    // process
+    for (int i = 1; i <= n; i++){
+        if(colors[i]==0){
             dfs(i, 1);
-            if (!is_bipartite)
-                break;
         }
     }
 
+    // output
     if (!is_bipartite) {
         cout << "IMPOSSIBLE" << nl;
         return;
     }
-
     for (int i = 1; i <= n; i++) {
-        cout << color[i] << sp;
+        cout << colors[i] << " ";
     }
+    cout << nl;
 }
 
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
     int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    // cin >> tc;
+    while (tc--) {
+        solve();
+    }
     return 0;
 }
