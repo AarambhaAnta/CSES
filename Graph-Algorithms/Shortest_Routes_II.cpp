f9d9608b1d1042a1d2ca74416276ea83e4e767c0
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:46:40 +0530

    feat: solve Shortest_Routes_II

--- Graph-Algorithms/Shortest_Routes_II.cpp
+++ Graph-Algorithms/Shortest_Routes_II.cpp
@@ -1,159 +1,55 @@
-
 #include <bits/stdc++.h>
-using namespace std;
-
-// Type Aliases
-#define int long long
-#define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+signed main() {
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-}
 
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
+    ll n, m, q;
+    cin >> n >> m >> q;
 
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
+    ll inf = 1e18;
+    vector<vector<ll>> dis(n + 1, vector<ll>(n + 1, inf));
+
+    for (ll i = 0; i < m; i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        dis[u][v] = min(dis[u][v], w);
+        dis[v][u] = min(dis[v][u], w);
     }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
+
+    for (ll i = 1; i <= n; i++) {
+        dis[i][i] = 0;
     }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-const int maxn = 505;
-int n, m, q;
-int dis[maxn][maxn];
-void flloydWarshal() {
-    for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (dis[i][k] < INF && dis[k][j] < INF) {
+    for (ll k = 1; k <= n; k++) {
+        for (ll i = 1; i <= n; i++) {
+            for (ll j = 1; j <= n; j++) {
+                if (dis[i][k] != inf && dis[k][j] != inf) {
                     dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
                 }
             }
         }
     }
-}
-int query(int a, int b) {
-    if (dis[a][b] >= INF || dis[a][a] < 0 || dis[b][b] < 0)
-        return -1;
-    return dis[a][b];
-}
-void test() {
-    // your solution for each testcase
-    cin >> n >> m >> q;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (i == j)
-                dis[i][j] = 0;
-            else
-                dis[i][j] = INF;
-        }
-    }
-
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        dis[u][v] = min(dis[u][v], w);
-        dis[v][u] = min(dis[v][u], w);
-    }
-    flloydWarshal();
 
     while (q--) {
-        int a, b;
-        cin >> a >> b;
-        cout << query(a, b) << nl;
+        ll u, v;
+        cin >> u >> v;
+        ll d = dis[u][v];
+        if (d == inf || d < 0) {
+            cout << -1 << '\n';
+        } else {
+            cout << d << '\n';
+        }
     }
-}
-
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
     return 0;
 }
