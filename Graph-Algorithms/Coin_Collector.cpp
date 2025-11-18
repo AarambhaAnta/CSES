403150411b8d625d8827883ec29069ed7c6f504b
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 11:26:46 +0530

    feat: solve Coin collector

--- Graph-Algorithms/Coin_Collector.cpp
+++ Graph-Algorithms/Coin_Collector.cpp
@@ -11,10 +11,9 @@ using ordered_set =
 typedef long long ll;
 const ll maxn = 2e5 + 5;
 ll n, m;
-vector<ll> g[maxn], rev_g[maxn];
+vector<ll> g[maxn], gRev[maxn];
 ll coins[maxn];
 ll visited[maxn];
-
 vector<ll> tout_order;
 
 void dfs1(ll u) {
@@ -26,31 +25,20 @@ void dfs1(ll u) {
     tout_order.push_back(u);
 }
 
-ll cur_scc = 0;
-ll scc_number[maxn];
+ll curScc = 0;
+ll scc[maxn];
 void dfs2(ll u) {
     visited[u] = 1;
-    scc_number[u] = cur_scc;
-    for (ll v : rev_g[u]) {
-        if (!visited[v]) {
+    scc[u] = curScc;
+    for (ll v : gRev[u]) {
+        if (!visited[v])
             dfs2(v);
-        }
     }
 }
 
-set<ll> condensed_graph[maxn];
-ll condensed_coins[maxn];
+ll coinsCondense[maxn];
 ll dp[maxn];
-
-void dfs3(ll u) {
-    visited[u] = 1;
-    for (ll v : condensed_graph[u]) {
-        if (!visited[v]) {
-            dfs3(v);
-        }
-    }
-    tout_order.push_back(u);
-}
+vector<ll> gCondense[maxn];
 
 signed main() {
     ios::sync_with_stdio(0);
@@ -60,54 +48,58 @@ signed main() {
     for (ll i = 1; i <= n; i++) {
         cin >> coins[i];
     }
+
     for (ll i = 0; i < m; i++) {
         ll u, v;
         cin >> u >> v;
         g[u].push_back(v);
-        rev_g[v].push_back(u);
+        gRev[v].push_back(u);
     }
 
-    for (ll u = 1; u <= n; u++) {
-        if (!visited[u])
-            dfs1(u);
+    for (ll i = 1; i <= n; i++) {
+        if (!visited[i])
+            dfs1(i);
     }
+
     reverse(all(tout_order));
     fill(visited, visited + n + 1, 0);
 
-    for (ll u : tout_order) {
-        if (!visited[u]) {
-            cur_scc++;
-            dfs2(u);
+    for (ll v : tout_order) {
+        if (!visited[v]) {
+            curScc++;
+            dfs2(v);
         }
     }
 
+
     for (ll u = 1; u <= n; u++) {
+        coinsCondense[scc[u]] += coins[u];
         for (ll v : g[u]) {
-            if (scc_number[u] != scc_number[v]) {
-                condensed_graph[scc_number[u]].insert(scc_number[v]);
+            if (scc[u] != scc[v]) {
+                gCondense[scc[u]].push_back(scc[v]);
             }
         }
-        condensed_coins[scc_number[u]] += coins[u];
     }
 
-    fill(visited, visited + n + 1, 0);
-    tout_order.clear();
+    vector<ll> scc_topo;
 
-    for (ll u = 1; u <= cur_scc; u++) {
-        if (!visited[u]) {
-            dfs3(u);
+    for (ll node : tout_order) {
+        ll s = scc[node];
+        if (scc_topo.empty() || scc_topo.back() != s) {
+            scc_topo.push_back(s);
         }
     }
-    reverse(all(tout_order));
 
-    // for (ll u : tout_order) {
-    //     cout << u << " ";
-    // }
-    // cout << endl;
-    ll ans = 0;
-    for (ll i = 1; i <= cur_scc; i++) {
-        ans = max(ans, dp[i]);
+    for (ll i = scc_topo.size() - 1; i >= 0; i--) {
+        ll u = scc_topo[i];
+        dp[u] = coinsCondense[u];
+        for (ll v : gCondense[u]) {
+            dp[u] = max(dp[u], coinsCondense[u] + dp[v]);
+        }
     }
-    cout << ans << endl;
+
+    ll maxCoins = *max_element(dp, dp + curScc + 1);
+    cout << maxCoins << '\n';
+
     return 0;
 }
