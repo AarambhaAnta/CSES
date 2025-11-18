eb28662a9bd62432f572cdc767efb19a9b74aae3
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:23 +0530

    feat: solve Cycle_Finding

--- Graph-Algorithms/Cycle_Finding.cpp
+++ Graph-Algorithms/Cycle_Finding.cpp
@@ -1,170 +1,67 @@
-
 #include <bits/stdc++.h>
-using namespace std;
-
-// Type Aliases
-#define int long long
-#define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
+typedef long long ll;
+
+signed main() {
+    ios::sync_with_stdio(0);
     cin.tie(nullptr);
-}
 
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
+    ll n, m;
+    cin >> n >> m;
+    ll inf = 1e18;
+    vector<tuple<ll, ll, ll>> edges;
+    for (ll i = 0; i < m; i++) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({u, v, w});
     }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
+    vector<ll> dis(n + 1);
+    vector<ll> parent(n + 1);
+    optional<int> path_end;
 
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const array<int, 4> dx = {0, 1, 0, -1};
-const array<int, 4> dy = {1, 0, -1, 0};
-const array<char, 4> dir = {'R', 'D', 'L', 'U'};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
-
-struct Edge {
-    int u, v, w;
-};
-int n, m;
-vector<Edge> edges;
-vi dis, parent;
-
-void bellman_ford(int src) {
-    dis.assign(n + 1, INF);
-    parent.assign(n + 1, -1);
-    dis[src] = 0;
-    for (int i = 1; i <= n; i++) {
-        for (const Edge e : edges) {
-            int u = e.u, v = e.v, w = e.w;
-            if (dis[v] > dis[u] + w) {
-                dis[v] = dis[u] + w;
+    for (ll i = 1; i <= n; i++) {
+        for (auto [u, v, w] : edges) {
+            ll new_dis = dis[u] + w;
+            if (new_dis < dis[v]) {
+                dis[v] = new_dis;
                 parent[v] = u;
-
                 if (i == n) {
-                    int x = v;
-                    for (int i = 1; i <= n; i++) {
-                        x = parent[x];
-                    }
-
-                    vi cycle;
-                    int cur = x;
-                    cycle.pb(cur);
-                    cur = parent[cur];
-                    while (cur != x) {
-                        cycle.pb(cur);
-                        cur = parent[cur];
-                    }
-                    cycle.pb(x);
-                    reverse(all(cycle));
-
-                    cout << "YES" << nl;
-                    vout(cycle);
-                    return;
+                    path_end = v;
                 }
             }
         }
     }
-    cout << "NO" << nl;
-}
 
-void test() {
-    // your solution for each testcase
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        edges.pb({u, v, w});
+    if (!path_end) {
+        cout << "NO" << endl;
+        return 0;
     }
 
-    bellman_ford(1);
-}
+    vector<bool> vis(n + 1, false);
+    vector<int> res;
+    for (int v = *path_end;; v = parent[v]) {
+        res.push_back(v);
+        if (vis[v])
+            break;
+        vis[v] = true;
+    }
+    reverse(all(res));
+    cout << "YES" << endl;
+    for (int i = 0; i < (int)res.size(); i++) {
+        cout << res[i] << " ";
+        if (i > 0 && res[i] == res.front())
+            break;
+    }
 
-//======================================== Driver Code ========================================//
-signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+    cout << endl;
+
     return 0;
 }
