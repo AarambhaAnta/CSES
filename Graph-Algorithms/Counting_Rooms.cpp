4d0128e88ff1934cba71f927d037a891d1b409dd
AarambhaAnta <aarambha108@gmail.com>
2025-11-18 09:45:08 +0530

    feat: solve Counting_Rooms

--- Graph-Algorithms/Counting_Rooms.cpp
+++ Graph-Algorithms/Counting_Rooms.cpp
@@ -1,152 +1,53 @@
-
 #include <bits/stdc++.h>
 using namespace std;
-
-// Type Aliases
-#define int long long
 #define nl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
 
-// I/O Fast
-void fastIO() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-// Debug Macro
-#ifndef ONLINE_JUDGE
-#define debug(x)                                                                                   \
-    cerr << #x << " = ";                                                                           \
-    _print(x);                                                                                     \
-    cerr << nl;
-#else
-#define debug(x)
-#endif
-
-// Debug Utilities
-void _print(int x) {
-    cerr << x;
-}
-void _print(string x) {
-    cerr << x;
-}
-void _print(char x) {
-    cerr << x;
-}
-void _print(bool x) {
-    cerr << (x ? "true" : "false");
-}
-void _print(double x) {
-    cerr << x;
-}
-template <typename T, typename V>
-void _print(pair<T, V> p) {
-    cerr << "{";
-    _print(p.ff);
-    cerr << ",";
-    _print(p.ss);
-    cerr << "}";
-}
-template <typename T>
-void _print(vector<T> v) {
-    cerr << "[";
-    for (T i : v) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "]";
-}
-template <typename T>
-void _print(set<T> s) {
-    cerr << "{";
-    for (T i : s) {
-        _print(i);
-        cerr << " ";
-    }
-    cerr << "}";
-}
-
-// Constants
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-const double EPS = 1e-9;
-const int dx[] = {0, 1, 0, -1};
-const int dy[] = {1, 0, -1, 0};
-
-// Aliases
-using vi = vector<int>;
-using vb = vector<bool>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-
-// Input / Output helpers
-template <typename T>
-void vin(vector<T>& a) {
-    for (auto& x : a)
-        cin >> x;
-}
-template <typename T>
-void vout(const vector<T>& a) {
-    for (auto x : a)
-        cout << x << sp;
-    cout << nl;
-}
-
-
-//======================================== Solution Logic ========================================//
+// initial
 int n, m;
-vector<string> grid;
-vector<vb> vis;
-bool isValid(int i, int j) {
-    return i >= 0 && j >= 0 && i < n && j < m;
-}
-vii nei(int i, int j) {
-    vii res;
-    for (int k = 0; k < 4; k++) {
-        int nx = i + dx[k];
-        int ny = j + dy[k];
-        if (isValid(nx, ny) && grid[nx][ny] == '.')
-            res.push_back({nx, ny});
-    }
-    return res;
-}
-void dfs(int i, int j) {
-    vis[i][j] = true;
-    for (auto [a, b] : nei(i, j)) {
-        if (!vis[a][b])
-            dfs(a, b);
-    }
-}
-void test() {
-    // your solution for each testcase
-    cin >> n >> m;
-    grid.assign(n, "");
-    vin(grid);
-    vis.assign(n, vb(m, false));
+vector<string> g;
+int current_rooms = 0;
+int row[] = {-1, 1, 0, 0};
+int col[] = {0, 0, -1, 1};
 
-    int rooms = 0;
+// helpers
+bool is_valid(int nx, int ny) {
+    return nx >= 0 && ny >= 0 && nx < n && ny < m;
+}
+void dfs(int x, int y) {
+    g[x][y] = 'm';
+    for (int i = 0; i < 4; i++) {
+        int nx = x + row[i], ny = y + col[i];
+        if (is_valid(nx, ny) && g[nx][ny] == '.') {
+            dfs(nx, ny);
+        }
+    }
+}
+
+void solve() {
+    // input
+    cin >> n >> m;
+    g.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> g[i];
+    }
+
+    // process
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (!vis[i][j] && grid[i][j] == '.') {
-                rooms++;
+            if (g[i][j] == '.') {
+                current_rooms++;
                 dfs(i, j);
             }
         }
     }
-    cout << rooms << nl;
+
+    // output
+    cout << current_rooms << nl;
 }
 
-// Driver Code
-signed main() {
-    fastIO();
-    int tc = 1;
-    // cin >> tc; // Uncomment for multiple test cases
-    while (tc--)
-        test();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
     return 0;
 }
