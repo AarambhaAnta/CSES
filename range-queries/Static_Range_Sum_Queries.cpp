6085a5f7cecffdeaead10cfb80ff9a0fe6818ff5
AarambhaAnta <aarambha108@gmail.com>
2025-12-20 16:29:18 +0530

    feat: solve static range sum queries

--- range-queries/Static_Range_Sum_Queries.cpp
+++ range-queries/Static_Range_Sum_Queries.cpp
@@ -1,34 +1,41 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define all(x) (x).begin(), (x).end()
 using namespace std;
-#define int long long
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-void solve() {
-    int n, q;
-    cin >> n >> q;
-    vector<int> A(n);
-    for (int i = 0; i < n; i++) {
-        cin >> A[i];
-        if (i != 0) {
-            A[i] += A[i - 1];
-        }
-    }
-    while (q--) {
-        int a, b;
-        cin >> a >> b;
-        --a, --b;
-        int v = A[b] - (a - 1 >= 0 ? A[a - 1] : 0);
-        cout << v << endl;
-    }
-}
+typedef long long ll;
 
-int32_t main() {
+signed main() {
     ios::sync_with_stdio(0);
     cin.tie(nullptr);
 
-    int tc = 1;
-    // cin >> tc;
-    while (tc--) {
-        solve();
+    int n, q;
+    cin >> n >> q;
+    vector<ll> A(n + 1);
+    
+    for (int i = 1; i <= n; i++) {
+        cin >> A[i];
     }
+
+    // prefix sum
+    for (int i = 1; i <= n; i++) {
+        A[i] += A[i - 1];
+    }
+
+    int l, r;
+    ll sum;
+    while (q--) {
+        cin >> l >> r;
+
+        sum = A[r] - A[l - 1];
+        cout << sum << '\n';
+    }
+
+
     return 0;
 }
